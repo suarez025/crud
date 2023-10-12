5de862e00d4cc3b514f5c3c9b19da9b587272e15
XxMaxiDiazxX <johansebastianvelezortiz@gmail.com>
2023-10-12 13:26:59 -0500

    poniendo la zona de las imagenes

--- js/app.js
+++ js/app.js
@@ -41,7 +41,8 @@ const cargarTabla = () => {
     const celdas = `<th>${item.gift}</th>
         <td>${item.tipo}</td>
         <td>${item.tiempo}</td>
-        <td>$${item.precio}</td>
+        <td>${item.precio}</td>
+        <td>${item.imagen}</td>
         <td>
         <div class="d-flex gap-2">
         <button class="btn btn-outline-warning" onclick="mostrarModal(${item.id})"><i class="fa fa-pencil" aria-hidden="true"></i></button>
